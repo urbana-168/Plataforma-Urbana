94fd5b35cc2ec9320a1fdb8f1b2a6f6d42380faf
Kareeeevv <karev.becerra19@gmail.com>
2026-06-26 13:36:49 -0600

    Pestaña 'Docentes' v1.0 finalizada. Corrección y reorganización de Scripts.

---
+++ assets/scripts/script-66F6F746572.js
@@ -0,0 +1,71 @@
+//@ts-check
+
+import { verifyEmail  } from "./utility/verifyEmail.js";
+
+/**
+ * Newsletter subscription function
+ */
+export function _6E6577736C6574746572() {
+    const botonMail = /** @type {HTMLButtonElement | null} */(document.getElementById("submit_email"));
+    /** @type {HTMLInputElement | null} */
+    const inputMail = document.querySelector("input[type='email']");
+    const modalSuccess = /** @type {HTMLDivElement | null} */(document.getElementById("modal_mail_registrado"));
+
+    if (botonMail && inputMail && modalSuccess){
+        inputMail.addEventListener("keydown", (event) => {
+            if (event.key === "Enter") {
+                event.preventDefault();
+
+                if (inputMail.value === "") {
+                    alert("No has escrito ningún correo en el campo.");
+    
+                } else {
+                    if (verifyEmail(inputMail.value)) {
+                        botonMail.disabled = true;
+                        botonMail.style.pointerEvents = "none";
+                        botonMail.innerText = "Registrado!";
+                        botonMail.style.color = "#2f2f2f";
+                        botonMail.style.backgroundColor = "#4d4d4d";
+                        botonMail.style.boxShadow = "#3b3b3b 0 3px 3px 3px";
+                        botonMail.style.border = "2px solid gray";
+                        inputMail.value = "";
+    
+                        modalSuccess.classList.toggle("show");
+                        setTimeout(() => {
+                            modalSuccess.classList.toggle("show");
+                        }, 3000);
+    
+                    } else {
+                        alert("El texto ingresado no es un correo válido.");
+                    }
+                }
+            }
+        });
+        
+        botonMail.addEventListener("click", () => {
+            if (inputMail.value === "") {
+                alert("No has escrito ningún correo en el campo.");
+
+            } else {
+                if (verifyEmail(inputMail.value)) {
+                    botonMail.disabled = true;
+                    botonMail.style.pointerEvents = "none";
+                    botonMail.innerText = "Registrado!";
+                    botonMail.style.color = "#2f2f2f";
+                    botonMail.style.backgroundColor = "#4d4d4d";
+                    botonMail.style.boxShadow = "#3b3b3b 0 3px 3px 3px";
+                    botonMail.style.border = "2px solid gray";
+                    inputMail.value = "";
+
+                    modalSuccess.classList.toggle("show");
+                    setTimeout(() => {
+                        modalSuccess.classList.toggle("show");
+                    }, 3000);
+
+                } else {
+                    alert("El texto ingresado no es un correo válido.");
+                }
+            }
+        });
+    }
+}
--- assets/scripts/scripts-A01.js
+++ assets/scripts/scripts-A01.js
@@ -1,36 +1,5 @@
 //@ts-check
-import {verifyEmail} from "./utility/verifyEmail.js";
-
+import { _6E6577736C6574746572 } from "./script-66F6F746572.js";
 document.addEventListener("DOMContentLoaded", () => {
-    const botonMail = /** @type {HTMLButtonElement | null} */(document.getElementById("submit_email"));
-    /** @type {HTMLInputElement | null} */
-    const inputMail = document.querySelector("input[type='email']");
-    const modalSuccess = /** @type {HTMLDivElement | null} */(document.getElementById("modal_mail_registrado"));
-
-    if (botonMail && inputMail && modalSuccess)
-    botonMail.addEventListener("click", () => {
-        if (inputMail.value === "") {
-            alert("No has escrito ningún correo en el campo.");
-
-        } else {
-            if (verifyEmail(inputMail.value)) {
-                botonMail.disabled = true;
-                botonMail.style.pointerEvents = "none";
-                botonMail.innerText = "Registrado!";
-                botonMail.style.color = "#2f2f2f";
-                botonMail.style.backgroundColor = "#4d4d4d";
-                botonMail.style.boxShadow = "#3b3b3b 0 3px 3px 3px";
-                botonMail.style.border = "2px solid gray";
-                inputMail.value = "";
-
-                modalSuccess.classList.toggle("show");
-                setTimeout(() => {
-                    modalSuccess.classList.toggle("show");
-                }, 3000);
-
-            } else {
-                alert("El texto ingresado no es un correo válido.");
-            }
-        }
-    });
+    _6E6577736C6574746572();
 });
